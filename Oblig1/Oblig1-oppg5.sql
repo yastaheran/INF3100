d658f5adb441be8dcd4641027936691aaa4cded4
yastaheran <y.taheranpour@gmail.com>
2018-11-18 12:31:45 +0100

    Update Oblig1-oppg5.sql

--- Oblig1/Oblig1-oppg5.sql
+++ Oblig1/Oblig1-oppg5.sql
@@ -1,17 +1,13 @@
 /* A. Skriv ut serietittel, produksjonsår og antall episoder for de yngste TVseriene i filmdatabasen 
-(dvs. de med størst verdi i attributtet firstprodyear).
-(i) en SQL-spørring */
+(dvs. de med størst verdi i attributtet firstprodyear). */
 
 select s.maintitle, s.firstprodyear, count(e.seriesid) as antEpisodes
 from series s left outer join episode e on s.seriesid = e.seriesid, (select max(firstprodyear) as 	yr from series) fpy
 where s.firstprodyear = fpy.yr
 group by s.maintitle, s.firstprodyear;
 
-(ii)
-
 /* B. Lag en liste over alle deltakelsestyper og hvor mange personer som prosentvis faller inn under hver deltakelsestype. 
-Listen skal være sortert etter fallende prosentpoeng. Ta med ett siffer etter desimaltegnet. 
-(i) en SQL-spørring  */
+Listen skal være sortert etter fallende prosentpoeng. Ta med ett siffer etter desimaltegnet.  */
 create view totantall as (select  count(personid) as totAntall from filmparticipation);
 select fp.parttype, round(count(fp.personid)*100.0/max(totAntall), 1) as prosent
 from filmparticipation fp, film f, totantall
@@ -19,11 +15,8 @@ where f.filmid = fp.filmid
 group by parttype
 order by prosent desc;
 
-(ii)
-
 /* C. Finn for- og etternavn på alle mannlige regissører som har laget mer enn 50 filmer, og der det er minst én kvinnelig 
-skuespiller som har vært med i samtlige filmer av denne regissøren. 
-(i) en SQL-spørring */
+skuespiller som har vært med i samtlige filmer av denne regissøren.  */
 
 select p.firstname, p.lastname
 from Person p natural join filmparticipation fp
@@ -33,5 +26,3 @@ where p.gender = 'M' and fp.parttype = 'director' and fp.filmid IN (select fp2.f
 group by p.firstname, p.lastname
 having count(fp.filmid) > 50
 order by p.lastname, p.firstname; 
-
-(ii)
